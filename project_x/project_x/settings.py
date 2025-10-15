2a57f647498430072804c57702f4346c4def55d9
Bernard411 <hamiebernard@gmail.com>
2025-10-15 03:44:40 -0500

    j

--- project_x/project_x/settings.py
+++ project_x/project_x/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-macs)$cs7&qe+$^nai(@b+dw7$tm5m7@@082w$5ycwryzikn_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
